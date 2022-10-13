ce4cd1b356520b4058f8af0c4153ad312a8f4e02
Pavel Morozuk <morozuki@mail.ru>
2022-10-13 22:18:29 +0300

    add simple navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 import { PATH } from './enams';
 import './App.css';
@@ -14,20 +14,31 @@ import {
 } from './pages';
 
 const ROUTS = [
+  { path: PATH.ROOT, element: <Login /> },
   { path: PATH.LOGIN, element: <Login /> },
   { path: PATH.NEW_PASSWORD, element: <NewPassword /> },
   { path: PATH.PAGE_NOT_FOUND, element: <PageNotFound /> },
   { path: PATH.PROFILE, element: <Profile /> },
   { path: PATH.REGISTRATION, element: <Registration /> },
   { path: PATH.PASSWORD_RECOVERY, element: <PasswordRecovery /> },
+  { path: PATH.BAD_PATH, element: <PageNotFound /> },
 ];
 
 const App = (): ReactElement => (
-  <Routes>
-    {ROUTS.map(({ path, element }) => (
-      <Route path={path} element={element} key={path} />
-    ))}
-  </Routes>
+  <>
+    <div>
+      <NavLink to={PATH.LOGIN}>- Login -</NavLink>
+      <NavLink to={PATH.NEW_PASSWORD}>- NewPassword -</NavLink>
+      <NavLink to={PATH.PROFILE}>- Profile -</NavLink>
+      <NavLink to={PATH.REGISTRATION}>- Registration -</NavLink>
+      <NavLink to={PATH.PASSWORD_RECOVERY}>- PasswordRecovery -</NavLink>
+    </div>
+    <Routes>
+      {ROUTS.map(({ path, element }) => (
+        <Route path={path} element={element} key={path} />
+      ))}
+    </Routes>
+  </>
 );
 
 export default App;
--- src/enams/paths.ts
+++ src/enams/paths.ts
@@ -1,8 +1,10 @@
 export enum PATH {
+  ROOT = '/',
   LOGIN = 'login',
   NEW_PASSWORD = 'new-password',
   PAGE_NOT_FOUND = 'page-not-found',
   PASSWORD_RECOVERY = 'password-recovery',
   PROFILE = 'profile',
   REGISTRATION = 'registration',
+  BAD_PATH = '*',
 }
